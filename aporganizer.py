a09aaa7cda0083d745cc3ebe28aef1acd2d185b3
Brian Lindner <brian@bclindner.com>
2025-11-15 15:58:19 -0500

    feat: first working version

---
+++ aporganizer.py
@@ -0,0 +1,134 @@
+from typing import Any
+from dataclasses import dataclass
+import sqlite3
+import yaml
+
+@dataclass
+class Yaml:
+    yaml_id: int
+    creator_id: str
+    slot_name: str
+    data: bytes
+
+@dataclass
+class APWorld:
+    apworld_id: int
+    creator_id: str
+    game_name: str
+    data: bytes
+
+def read_slot_name(data: bytes) -> str:
+    try:
+        return yaml.load(data, Loader=yaml.SafeLoader)["name"]
+    except Exception as e:
+        raise InvalidYamlException(e)
+
+class InvalidYamlException(BaseException):
+    pass
+
+class AlreadyExistsException(BaseException):
+    pass
+
+class NotExistsException(BaseException):
+    pass
+
+class APOrganizer:
+    def __init__(self, *args: Any, **kwargs: dict[str, Any]):
+        self.db: sqlite3.Connection = sqlite3.connect(*args, **kwargs)
+        self.initialize_db()
+    def initialize_db(self):
+        cursor = self.db.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS yaml (
+                yaml_id INTEGER NOT NULL PRIMARY KEY,
+                creator_id TEXT NOT NULL,
+                slot_name TEXT NOT NULL UNIQUE,
+                data BLOB NOT NULL
+            );
+        """)
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS apworld (
+                apworld_id INTEGER NOT NULL PRIMARY KEY,
+                creator_id TEXT NOT NULL,
+                game_name TEXT NOT NULL UNIQUE,
+                data BLOB NOT NULL
+            );
+        """)
+        self.db.commit()
+    def add_yaml(self, creator_id: str, data: bytes):
+        """Add a YAML to the database."""
+        slot_name = read_slot_name(data)
+        cursor = self.db.cursor()
+        # check if creator/slot has already been set. if so, delete it
+        existing_yaml_id = self._yaml_exists(cursor, creator_id, slot_name)
+        if existing_yaml_id is not None:
+            cursor.execute("DELETE FROM yaml WHERE yaml_id = ?", (existing_yaml_id,))
+        try:
+            cursor.execute("INSERT INTO yaml (creator_id, slot_name, data) VALUES (?,?,?)", (creator_id,slot_name,data))
+        except sqlite3.IntegrityError:
+            raise AlreadyExistsException(f"Slot name {slot_name} already exists")
+        self.db.commit()
+        return Yaml(
+                yaml_id=cursor.lastrowid,
+                creator_id=creator_id,
+                slot_name=slot_name,
+                data=data,
+        )
+    def _yaml_exists(self, cursor: sqlite3.Cursor, creator_id: str, slot_name: str):
+        result = cursor.execute("SELECT yaml_id FROM yaml WHERE creator_id = ? AND slot_name = ?", (creator_id, slot_name)).fetchone()
+        if result is None:
+            return None
+        return result[0]
+    def get_yamls(self):
+        cursor = self.db.cursor()
+        for row in cursor.execute("SELECT yaml_id, creator_id, slot_name, data FROM yaml"):
+            yaml_id, creator_id, slot_name, data = row
+            yield Yaml(
+                yaml_id=yaml_id,
+                creator_id=creator_id,
+                slot_name=slot_name,
+                data=data,
+            )
+    def delete_yaml(self, slot_name: str):
+        cursor = self.db.cursor()
+        cursor.execute("DELETE FROM yaml WHERE slot_name = ?", (slot_name,))
+        self.db.commit()
+        if cursor.rowcount == 0:
+            raise NotExistsException()
+
+    def add_apworld(self, creator_id: str, game_name: str, data: bytes):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute("INSERT INTO apworld (creator_id, game_name, data) VALUES (?,?,?)",(creator_id,game_name,data))
+        except sqlite3.IntegrityError:
+            raise AlreadyExistsException(f"APWorld for {game_name} already submitted")
+        self.db.commit()
+        return APWorld(
+            apworld_id=cursor.lastrowid,
+            creator_id=creator_id,
+            game_name=game_name,
+            data=data,
+        )
+
+    def get_apworlds(self):
+        cursor = self.db.cursor()
+        for row in cursor.execute("SELECT apworld_id, creator_id, game_name, data FROM apworld"):
+            yield APWorld(
+                apworld_id=row[0],
+                creator_id=row[1],
+                game_name=row[2],
+                data=row[3],
+            )
+
+    def delete_apworld(self, game_name: str):
+        cursor = self.db.cursor()
+        cursor.execute("DELETE FROM apworld WHERE game_name = ?", (game_name,))
+        self.db.commit()
+        if cursor.rowcount == 0:
+            raise NotExistsException()
+
+    def clear(self):
+        cursor = self.db.cursor()
+        cursor.execute("DELETE FROM yaml")
+        cursor.execute("DELETE FROM apworld")
+        self.db.commit()
---
+++ test_aporganizer.py
@@ -0,0 +1,90 @@
+import pytest
+import aporganizer
+
+TEST_CREATOR = "0123456789"
+TEST_CREATOR_2 = "987543210"
+TEST_YAML = b"name: Testing"
+TEST_YAML_2 = b"name: Testing\r\ndata: asdf"
+TEST_SLOT_NAME = "Testing"
+TEST_GAME = "Bingly 3"
+TEST_APWORLD = b"asdf"
+
+@pytest.fixture()
+def organizer():
+    return aporganizer.APOrganizer(":memory:")
+
+def test_read_slot_name():
+    slot_name = aporganizer.read_slot_name(TEST_YAML)
+    assert slot_name == TEST_SLOT_NAME
+
+@pytest.mark.parametrize('data', [
+    b"!*$(^&%(#*", # non-yaml data
+    b"mame: Testing" # "name" key not in yaml
+])
+def test_read_slot_name_bad(data):
+    with pytest.raises(aporganizer.InvalidYamlException):
+        aporganizer.read_slot_name(data)
+
+def test_add_yaml(organizer):
+    yaml = organizer.add_yaml(TEST_CREATOR, TEST_YAML)
+    assert yaml.slot_name == TEST_SLOT_NAME
+
+def test_add_yaml_overwrite(organizer):
+    organizer.add_yaml(TEST_CREATOR, TEST_YAML)
+    organizer.add_yaml(TEST_CREATOR, TEST_YAML_2)
+    yamls = list(organizer.get_yamls())
+    assert len(yamls) == 1
+    assert yamls[0].data == TEST_YAML_2
+
+def test_add_yaml_overwrite_bad(organizer):
+    organizer.add_yaml(TEST_CREATOR, TEST_YAML)
+    with pytest.raises(aporganizer.AlreadyExistsException):
+        organizer.add_yaml(TEST_CREATOR_2, TEST_YAML)
+
+def test_get_yamls_none(organizer):
+    assert len(list(organizer.get_yamls())) == 0
+
+def test_get_yamls_one(organizer):
+    yaml = organizer.add_yaml(TEST_CREATOR, TEST_YAML)
+    yamls = list(organizer.get_yamls())
+    assert len(yamls) == 1
+    assert yaml.yaml_id == yamls[0].yaml_id
+    assert yaml.slot_name == yamls[0].slot_name
+    assert yaml.creator_id == yamls[0].creator_id
+    assert yaml.data == yamls[0].data
+
+def test_delete_yaml(organizer):
+    yaml = organizer.add_yaml(TEST_CREATOR, TEST_YAML)
+    organizer.delete_yaml(yaml.slot_name)
+    assert len(list(organizer.get_yamls())) == 0
+
+def test_delete_yaml_nonexistent(organizer):
+    with pytest.raises(aporganizer.NotExistsException):
+        organizer.delete_yaml("slot")
+
+def test_add_apworld(organizer):
+    apworld = organizer.add_apworld(TEST_CREATOR, TEST_GAME, TEST_APWORLD)
+    assert apworld.apworld_id != None
+    assert apworld.creator_id == TEST_CREATOR
+    assert apworld.game_name == TEST_GAME
+    assert apworld.data == TEST_APWORLD
+
+
+def test_get_apworlds_none(organizer):
+    assert len(list(organizer.get_apworlds())) == 0
+
+def test_get_apworlds_one(organizer):
+    apworld = organizer.add_apworld(TEST_CREATOR, TEST_GAME, TEST_APWORLD)
+    apworlds = list(organizer.get_apworlds())
+    assert len(apworlds) == 1
+    assert apworld.apworld_id == apworlds[0].apworld_id
+    assert apworld.creator_id == apworlds[0].creator_id 
+    assert apworld.game_name == apworlds[0].game_name 
+    assert apworld.data == apworlds[0].data
+
+def test_clear(organizer):
+    organizer.add_yaml(TEST_CREATOR, TEST_YAML)
+    organizer.add_apworld(TEST_CREATOR, TEST_GAME, TEST_APWORLD)
+    organizer.clear()
+    assert len(list(organizer.get_yamls())) == 0
+    assert len(list(organizer.get_apworlds())) == 0
